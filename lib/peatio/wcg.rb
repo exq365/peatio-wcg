41b3634497ac43bb6045f7dcce1351c48c6c5fcc
Dinesh Chohda <dinesh.skyach@gmail.com>
2018-10-25 17:16:25 +0530

    bundle gem peatio-wcg

---
+++ lib/peatio/wcg.rb
@@ -0,0 +1,7 @@
+require "peatio/wcg/version"
+
+module Peatio
+  module Wcg
+    # Your code goes here...
+  end
+end
---
+++ spec/peatio/wcg_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Peatio::Wcg do
+  it "has a version number" do
+    expect(Peatio::Wcg::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
